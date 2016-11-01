b53ac760ac5c1d0a615eb34c212fed585c3e06a1
Kelly Anderson <kelly.j.anderson@gmail.com>
2016-10-31 17:26:04 -0700

    Add flask-restful and update the hello react component to use fetch to load from the resource.

--- app/app.py
+++ app/app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
+from flask_restful import Resource, Api
 import os
 
 app = Flask(__name__)
+api = Api(app)
 
 app.config.from_object('config.default')
 if os.environ.get('APP_CONFIG_FILE'):
     app.config.from_envvar('APP_CONFIG_FILE')
+
+class Hello(Resource):
+    def get(self):
+        return {'message': u'Hello React World!'}
+
+api.add_resource(Hello, '/hello');
 
 @app.route('/')
 def root(name=None):
